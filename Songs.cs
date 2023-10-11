0528f2647c71bbab33b4806d5f5f46d34d18c422
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-11 04:03:58 +0200

    Add project files.

---
+++ Songs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Songs holiday = new Songs("Holiday", "Green Day", 1);
+        Songs kashmir = new Songs("kashmir", "Led Zippelin", 2);
+
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();
+        }
+    }
+}
